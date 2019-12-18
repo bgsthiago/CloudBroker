bede2ac7b355bae5782164d07c39df53fb29801c
Felipe Guimarães <luiz.guimaraes@dcomp.sor.ufscar.br>
2019-12-18 02:44:18 -0300

    client arrumado se pa

--- clients/cliente.py
+++ clients/cliente.py
@@ -1,47 +1,86 @@
-from flask import Flask, request, jsonify, render_template, redirect
-# from bson.objectid import ObjectId
 import json
-import threading
-import sys
 import requests
 
 provedor = Flask(__name__)
 # url = "https://cloudbroker2019.herokuapp.com/{route}"
 url = "http://localhost:5000/{route}"
 url_provedor = "http://localhost:{provedor}/{route}"
-
-# {
-#   "qtd_vcpu": 4,
-#   "qtd_ram": 16,
-#   "qtd_disco": 128
-# }
-
+maquinas_em_uso = []
 
 def procura():
+    vcpu = input('Insira uma quantidade mínima de vCPU que deseja: ')
+    ram = input('Insira uma quantidade mínima de RAM que deseja: ')
+    disco = input('Insira uma quantidade mínima de disco que deseja: ')
+
     with open('req.json', 'r') as req:
         r = requests.post(url.format(route='encontrar'), data=req)
-        return r.json(), r.status_code
+
+    if r.status_code == 201:
+        maquina = r.json()
+        vcpu = maquina['qtd_vcpu']
+        ram = maquina['qtd_ram']
+        disco = maquina['qtd_disco']
+        preco = maquina['preco']
+        provedor = maquina['provedor']
+        id = maquina['maquina']
+
+        print('Encontramos uma máquina com as seguintes especificações:')
+        print(f'Provedor: {provedor} - ID {id}')
+        print(f"vCPU's: {vcpu}")
+        print(f'RAM: {ram}')
+        print(f'disco: {disco}')
+        print(f'preco: {preco}')
 
 
-def usa(provedor, id):
+def usa():
+    provedor = ('Qual o provedor responsável pela máquina que deseja utilizar?\n> ')
+    id = input('Qual ID dá maquina que deseja utilizar?\n> ')
+
     r = requests.post(url_provedor.format(
         provedor=provedor, route='usar'), json={"id": id})
-    return r.json(), r.status_code
+
+    maquinas_em_uso.append((provedor, id))
+
+    print(f'A máquina com Provedor: {provedor} e ID: {id} está pronta para uso.\n')
 
 
-def libera(provedor, id):
+def libera():
+    provedor = ('Qual o provedor responsável pela máquina que deseja liberar?\n> ')
+    id = input('Qual ID dá maquina que deseja liberar?\n> ')
+
     r = requests.post(url_provedor.format(
         provedor=provedor, route='liberar'), json={"id": id})
-    return r.json(), r.status_code
+
+    maquinas_em_uso = list(filter(lambda x: x[0] not provedor and x[1] not id))
+
+    print(f'A máquina com Provedor: {provedor} e ID: {id} foi liberada com sucesso.\n')
+
+
+def listar():
+    print('Máquinas em uso:')
+    [ print(i) for i in maquinas_em_uso ]
+    print()
 
 
 if __name__ == "__main__":
-    x = procura()
-    print(x[0])
-    print(x[1])
-    if x[1] == 200:
-        print('oi')
-        print(usa(x[0]['provedor'], x[0]['maquina']))
-        print(libera(x[0]['provedor'], x[0]['maquina']))
+    while True:
 
-    #     provedor.run(host='localhost', debug=True, port=5015)
+        menu = 'Seleciona a opção desejada\n\n'
+        menu += '[1] - Encontrar máquina:\n'
+        menu += '[2] - Utilizar máquina:\n'
+        menu += '[3] - Listar máquinas em uso\n'
+        menu += '[4] - Liberar máquina\n'
+
+        option = input(menu)
+
+        menu_options: {
+            '1': procura(),
+            '2': usa(),
+            '3': listar(),
+            '4': libera(),
+        }
+
+        try: 
+            menu_options[option]
+        except:
+            print('Opção inválida\n')
