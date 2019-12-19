850fac1772cfbc74e36728cdf7e0f3273e587162
sublime <thiago.borges@gamersclub.com.br>
2019-12-19 10:23:13 -0300

    add app final heroku

--- app/app.py
+++ app/app.py
@@ -10,12 +10,12 @@ import os
 app = Flask(__name__)
 
 # db config nuvem
-# app.config['MONGO_DBNAME'] = 'heroku_ldzh9cl7'
-# app.config['MONGO_URI'] = 'mongodb://heroku_ldzh9cl7:ut7hph7s95u3lq9qf019p1lol3@ds137263.mlab.com:37263/heroku_ldzh9cl7?heroretryWrites=false'
+app.config['MONGO_DBNAME'] = 'heroku_ldzh9cl7'
+app.config['MONGO_URI'] = ''
 
 # db config local
 # app.config['MONGO_DBNAME'] = 'cloudbroker'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/cloudbroker'
+# app.config['MONGO_URI'] = 'mongodb://localhost:27017/cloudbroker'
 
 # db get
 mongo = PyMongo(app)
@@ -38,12 +38,6 @@ def lista():
 def acesso_provedor():
     received_data = loads(request.data.decode('utf-8'))
     print(received_data)
-    # ans = {
-    #        'qtd_vcpu': received_data['maquina']['qtd_vcpu'],
-    #        'qtd_ram': received_data['maquina']['qtd_ram'],
-    #        'qtd_disco': received_data['maquina']['qtd_disco'],
-    #        'preco': received_data['maquina']['preco'],
-    #        'em_uso': received_data['maquina']['em_uso']}
     try:
         for each in received_data['maquinas']:
 
@@ -53,9 +47,6 @@ def acesso_provedor():
     except Exception as e:
         print(e)
         return jsonify({'Message': 'Error.'}), 400
-
-    # print('CADASTRADO COM SUCESSO:\nvCPUS : ', vCPU, '\nRAM :',
-    #       RAM, 'GB\nHD :', HD, 'GB\nPREÇO R$', PRECO)
 
     return jsonify(dumps(received_data)), 201
 
@@ -75,8 +66,7 @@ def busca():
     try:
         print('Foi Achado essa VM:\n')
         print(a[0])
-        # msg = 'Foi Achado um com as seguintes configuraçoes:\nvCPUS : ', str(a[0]['vCPU']), '\nRAM :', str(a[0]['RAM']), '  GB\nHD :', str(
-        #   a[0]['HD']), '  GB\nPREÇO R$ ', str(a[0]['preco']), '\nLink de Acesso: 127.0.0.1:', str(a[0]['nome']), '\nMaquina : ', str(a[0]['id'])
+
         resposta = {'provedor': a[0]['nome'], 'maquina': a[0]['id'], 'preco': a[0]['preco'],
                     'qtd_vcpu': a[0]['qtd_vcpu'], 'qtd_ram': a[0]['qtd_ram'], 'qtd_disco': a[0]['qtd_disco']}
     except:
@@ -86,6 +76,6 @@ def busca():
 
 
 if __name__ == "__main__":
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True)
